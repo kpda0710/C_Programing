dea8b2bd9b8b53d9afd51479b26ff2ad92f4a42a
jung <42294848+kpda0710@users.noreply.github.com>
2023-09-15 13:44:35 +0900

    Add files via upload

---
+++ 2주차_과제.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+	int num1, num2;
+	printf("두개의 정수를 입력하세요: ");
+	scanf("%d %d", &num1, &num2);
+
+	printf("Input two integers: %d %d \n", num1, num2);
+	printf("%d & %d = %d \n", num1, num2, num1 & num2);
+	printf("%d | %d = %d \n", num1, num2, num1 | num2);
+	printf("%d ^ %d = %d \n", num1, num2, num1 ^ num2);
+	return 0;
+}
